57673aeb4c9465ab07ac7a6ab2e2002425d123dd
elenaIsla <elisfa28@gmail.com>
2019-04-11 22:15:22 +0200

    start with Clau

--- models/user.js
+++ models/user.js
@@ -2,8 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: String,
-  password: String,
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  city: {type: String},
+  neighbourhood: {type: String},
+  beerType: {type: String},  
+  favouriteBeers: [{type: ObjectId, ref: 'Beers'}], 
+  userimage: String,
 }, {
   timestamps: {
     createdAt: 'created_at',
